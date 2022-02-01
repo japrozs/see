ed1438ca70ace1473c40681c10e5bdbfe10746d6
Japroz Saini <sainijaproz@gmail.com>
2022-02-01 19:06:21 +0530

    :hammer: add cmake module. never touching that again!

---
+++ cmake/build/CMakeFiles/cmake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake.dir/main.cpp.o"
+  "CMakeFiles/cmake.dir/main.cpp.o.d"
+  "cmake"
+  "cmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
